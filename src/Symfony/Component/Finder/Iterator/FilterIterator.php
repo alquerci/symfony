be47f2f9670d034a9afdbcfc7967b592bdbb6bf1
alquerci <alquerci@email.com>
2013-05-22 19:38:06 +0200

    Revert modif under FilterIterator

--- src/Symfony/Component/Finder/Iterator/FilterIterator.php
+++ src/Symfony/Component/Finder/Iterator/FilterIterator.php
@@ -19,62 +19,6 @@
 abstract class Symfony_Component_Finder_Iterator_FilterIterator extends FilterIterator
 {
     /**
-     * @var Iterator
-     */
-    private $it;
-
-    public function __construct(Iterator $iterator)
-    {
-        $this->it = $iterator;
-    }
-
-    public function __call($func, $params)
-    {
-        return call_user_func_array(array($this->it, $func), $params);
-    }
-
-    protected function __clone()
-    {
-
-    }
-
-    public function current()
-    {
-        return $this->it->current();
-    }
-
-    public function key()
-    {
-        return $this->it->key();
-    }
-
-    public function valid()
-    {
-        return $this->it->valid();
-    }
-
-    protected function fetch()
-    {
-        while ($this->it->valid()) {
-            if ($this->accept()) {
-                return;
-            }
-            $this->it->next();
-        }
-    }
-
-    public function getInnerIterator()
-    {
-        return $this->it;
-    }
-
-    public function next()
-    {
-        $this->it->next();
-        $this->fetch();
-    }
-
-    /**
      * This is a workaround for the problem with \FilterIterator leaving inner \FilesystemIterator in wrong state after
      * rewind in some cases.
      *
@@ -91,7 +35,6 @@ abstract class Symfony_Component_Finder_Iterator_FilterIterator extends FilterIt
             $iterator = $iterator->getInnerIterator();
         }
 
-        $this->it->rewind();
-        $this->fetch();
+        parent::rewind();
     }
 }
