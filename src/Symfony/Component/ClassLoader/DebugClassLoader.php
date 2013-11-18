5db9c643e32e68925abe29e1b9d0c3c4f81dd2ed
alquerci <alquerci@email.com>
2013-11-18 08:36:42 +0100

    [ClassLoader] Fix DebugClassLoader for php bugs 40091 and 44144
      http://bugs.php.net/40091
      http://bugs.php.net/44144

--- src/Symfony/Component/ClassLoader/DebugClassLoader.php
+++ src/Symfony/Component/ClassLoader/DebugClassLoader.php
@@ -23,6 +23,9 @@
  */
 class Symfony_Component_ClassLoader_DebugClassLoader
 {
+    private static $classFinders = array();
+    private static $registered = false;
+
     private $classFinder;
 
     /**
@@ -46,13 +49,45 @@ class Symfony_Component_ClassLoader_DebugClassLoader
             return;
         }
 
-        foreach ($functions as $function) {
+        foreach ($functions as $key => $function) {
+            if (version_compare(phpversion(), '5.2.11', '<')) {
+                // http://bugs.php.net/44144
+                if (is_array($function) && is_object($function[0])) {
+                    $functions[$key] = null;
+
+                    continue;
+                }
+            }
+
             spl_autoload_unregister($function);
         }
 
         foreach ($functions as $function) {
-            if (is_array($function) && !$function[0] instanceof self && is_callable(array($function[0], 'findFile'))) {
-                $function = array(new self($function[0]), 'loadClass');
+            if (null === $function) {
+                continue;
+            }
+
+            if (is_array($function) && !$function[0] instanceof self && $function[0] !== 'Symfony_Component_ClassLoader_DebugClassLoader' && is_callable(array($function[0], 'findFile'))) {
+                $r = new ReflectionMethod($function[0], 'findFile');
+                if (1 === $r->getNumberOfRequiredParameters()) {
+                    $parameters = $r->getParameters();
+                    $param = $parameters[0];
+                    if (!$param->getClass() && !$param->isArray()) {
+                        // http://bugs.php.net/40091 and http://bugs.php.net/44144
+                        if (version_compare(phpversion(), '5.2.11', '<')) {
+                            self::$classFinders[] = $function[0];
+
+                            if (true === self::$registered) {
+                                continue;
+                            }
+                            self::$registered = true;
+
+                            $function = array(__CLASS__, 'staticLoadClass');
+                        } else {
+                            $function = array(new self($function[0]), 'loadClass');
+                        }
+                    }
+                }
             }
 
             spl_autoload_register($function);
@@ -91,6 +126,48 @@ class Symfony_Component_ClassLoader_DebugClassLoader
     public function loadClass($class)
     {
         if ($file = $this->findFile($class)) {
+            require $file;
+
+            if (!class_exists($class, false) && !interface_exists($class, false) && (!function_exists('trait_exists') || !trait_exists($class, false))) {
+                if (false !== strpos($class, '/')) {
+                    throw new RuntimeException(sprintf('Trying to autoload a class with an invalid name "%s". Be careful that the namespace separator is "\" in PHP, not "/".', $class));
+                }
+
+                throw new RuntimeException(sprintf('The autoloader expected class "%s" to be defined in file "%s". The file was found but the class was not in it, the class name or namespace probably has a typo.', $class, $file));
+            }
+
+            return true;
+        }
+    }
+
+    /**
+     * Finds a file by class name
+     *
+     * @param string $class A class name to resolve to file
+     *
+     * @return string|null
+     */
+    public static function staticfindFile($class)
+    {
+        foreach (self::$classFinders as $classFinder) {
+            if ($file = call_user_func(array($classFinder, 'findFile'), $class)) {
+                return $file;
+            }
+        }
+    }
+
+    /**
+     * Loads the given class or interface.
+     *
+     * @param string $class The name of the class
+     *
+     * @return Boolean|null True, if loaded
+     *
+     * @throws RuntimeException
+     */
+    public static function staticLoadClass($class)
+    {
+        if ($file = self::staticfindFile($class)) {
             require $file;
 
             if (!class_exists($class, false) && !interface_exists($class, false) && (!function_exists('trait_exists') || !trait_exists($class, false))) {
